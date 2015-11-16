4ede09a44c2a614f27da11251e7cb477a043589a
leeyoungmars <1469456432@qq.com>
2015-11-16 10:49:46 +0800

    添加修改信息功能

--- app/src/main/java/com/shyling/healthmanager/activity/PersonActivity.java
+++ app/src/main/java/com/shyling/healthmanager/activity/PersonActivity.java
@@ -1,7 +1,7 @@
 package com.shyling.healthmanager.activity;
 
-import android.app.Activity;
-import android.content.Intent;
+
+import android.content.ContentValues;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,31 +9,27 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageButton;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.shyling.healthmanager.R;
-import com.shyling.healthmanager.model.User;
 import com.shyling.healthmanager.util.DBHelper;
 import com.shyling.healthmanager.util.Utils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
  * Created by Mars on 2015/11/3.
  */
-public class PersonActivity extends AppCompatActivity {
+public class PersonActivity extends AppCompatActivity implements View.OnClickListener{
     ActionBar actionBar;
     TextView username;
     TextView name;
     TextView birthday;
-    TextView cellphone;
+    EditText cellphone;
+    Button modify_news,save_news;
+    DBHelper dbHelper;
+    SQLiteDatabase database;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,21 +39,45 @@ public class PersonActivity extends AppCompatActivity {
             //actionBar.setIcon(mipmap.ic_launcher);
             actionBar.setDisplayShowHomeEnabled(true);
             actionBar.setHomeButtonEnabled(true);
-            actionBar.setTitle("个人信息");
         }
         setContentView(R.layout.activity_person);
         username = (TextView) findViewById(R.id.tv_person_username);
         name = (TextView) findViewById(R.id.tv_person_name);
         birthday = (TextView) findViewById(R.id.tv_person_birthDay);
-        cellphone = (TextView) findViewById(R.id.tv_person_phone);
-        SharedPreferences sharedPreferences = getSharedPreferences("info",0);
-
-        DBHelper dbHelper = new DBHelper(this, "tb_userinfo.db", null, 1);
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        cellphone = (EditText) findViewById(R.id.et_person_phone);
+        cellphone.setEnabled(false);
+        modify_news = (Button) findViewById(R.id.modify_news);
+        save_news = (Button) findViewById(R.id.save_news);
+        save_news.setOnClickListener(this);
+        modify_news.setOnClickListener(this);
+        sharedPreferences = getSharedPreferences("info",0);
+        dbHelper = new DBHelper(this, "tb_userinfo.db", null, 1);
+        database = dbHelper.getWritableDatabase();
         Cursor cursor = database.query("tb_userinfo", null, "userName=?", new String[]{ sharedPreferences.getString("_username",null)}, null, null, null, null);
-        username.setText(cursor.getString(1));
-        name.setText(cursor.getString(3));
-        birthday .setText(cursor.getString(4));
-        cellphone.setText(cursor.getString(5));
+        if (cursor.moveToFirst()) {
+            username.setText(cursor.getString(1));
+            name.setText(cursor.getString(3));
+            birthday.setText(cursor.getString(4));
+            cellphone.setText(cursor.getString(5));
+        }
+        else {finish();}
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.modify_news:
+                cellphone.setEnabled(true);
+                Utils.Toast(PersonActivity.this,"修改信息");
+                break;
+            case R.id.save_news:
+                cellphone.setEnabled(false);
+                String ucellPhone = cellphone.getText().toString().trim();
+                ContentValues values = new ContentValues();
+                values.put("cellPhone",ucellPhone);
+                database.update("tb_userinfo", values, "userName=?", new String[]{sharedPreferences.getString("_username", null)});
+                database.close();
+                Utils.Toast(PersonActivity.this,"保存成功");
+        }
     }
 }
