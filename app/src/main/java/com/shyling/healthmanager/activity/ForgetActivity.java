361f4add21959ffad4750521f6d26a8b86e6f2ef
leeyoungmars <1469456432@qq.com>
2015-11-24 16:14:27 +0800

    完善修改密码

--- app/src/main/java/com/shyling/healthmanager/activity/ForgetActivity.java
+++ app/src/main/java/com/shyling/healthmanager/activity/ForgetActivity.java
@@ -1,15 +1,28 @@
 package com.shyling.healthmanager.activity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.shyling.healthmanager.R;
+import com.shyling.healthmanager.util.DBHelper;
+import com.shyling.healthmanager.util.Utils;
 
 
 /**
  * Created by Mars on 2015/11/4.
  */
 public class ForgetActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,33 @@ public class ForgetActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
         setContentView(R.layout.activity_forget);
+        Button btn_passwd = (Button) findViewById(R.id.forget_pwd);
+        btn_passwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    String passWd = ((EditText) findViewById(R.id.et_newpwd)).getText().toString();
+                    String repassWd = ((EditText) findViewById(R.id.et_renewpwd)).getText().toString();
+                    if(TextUtils.isEmpty(passWd)){
+                        Utils.Toast(ForgetActivity.this, "请输入密码");
+                    }else if(TextUtils.isEmpty(repassWd)){
+                        Utils.Toast(ForgetActivity.this, "请输入密码");
+                    }else if (!passWd.equals(repassWd)) {
+                            Toast.makeText(ForgetActivity.this, "两次输入密码不一样!", Toast.LENGTH_LONG).show();
+                            return;
+                    } else {
+                        sharedPreferences = getSharedPreferences("infopasswd", 0);
+                        DBHelper dbHelper = new DBHelper(ForgetActivity.this, "tb_userinfo.db", null, 1);
+                        SQLiteDatabase database = dbHelper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.put("passWd", repassWd);
+                        database.update("tb_userinfo", values, "cellPhone=?", new String[]{sharedPreferences.getString("_phone", null)});
+                        database.close();
+                        Utils.Toast(ForgetActivity.this, "修改密码成功！");
+                        Intent intent = new Intent();
+                        intent.setClass(ForgetActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            });
     }
 }
--- app/src/main/java/com/shyling/healthmanager/activity/ValidateActivity.java
+++ app/src/main/java/com/shyling/healthmanager/activity/ValidateActivity.java
@@ -1,19 +1,26 @@
 package com.shyling.healthmanager.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.shyling.healthmanager.R;
+import com.shyling.healthmanager.util.DBHelper;
+import com.shyling.healthmanager.util.Utils;
+
+import java.io.Serializable;
 
 
 /**
  * Created by Mars on 2015/11/4.
  */
-public class ValidateActivity extends AppCompatActivity implements View.OnClickListener{
+public class ValidateActivity extends AppCompatActivity{
     Button forget_next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +33,28 @@ public class ValidateActivity extends AppCompatActivity implements View.OnClickL
         }
         setContentView(R.layout.activity_validate);
         forget_next = (Button) findViewById(R.id.forget_next);
-        forget_next.setOnClickListener(this);
-        String vuser = ((EditText)findViewById(R.id.et_vuser)).getText().toString();
-    }
+        forget_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbHelper = new DBHelper(ValidateActivity.this,"tb_userinfo.db",null,1);
+                String vphone = ((EditText)findViewById(R.id.et_vphone)).getText().toString();
+                Cursor cursor = dbHelper.getReadableDatabase().rawQuery("select * from tb_userinfo where cellPhone like ?", new String[]{"%"
+                        + vphone + "%"});
+                if(TextUtils.isEmpty(vphone)){
+                    Utils.Toast(ValidateActivity.this, "请输入联系方式");
+                }else if (cursor.moveToFirst()) {
+                    SharedPreferences sharedPreferences = getSharedPreferences("infopasswd",0);
+                    SharedPreferences.Editor editor =sharedPreferences.edit();
+                    editor.putString("_phone", cursor.getString(5));
+                    editor.commit();
+                    Intent intent = new Intent();
+                    intent.setClass(ValidateActivity.this, ForgetActivity.class);
+                    startActivity(intent);
+                }else {
+                    Utils.Toast(ValidateActivity.this, "输入有误");
+                }
+            }
+        });
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.forget_next :
-                Intent Intent = new Intent();
-                Intent.setClass(this,ForgetActivity.class);
-                startActivity(Intent);
-                break;
-        }
     }
 }
