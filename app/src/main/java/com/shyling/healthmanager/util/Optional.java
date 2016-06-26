6a72b98d157f429bfbb1489f3d973b136a41a8e5
lingmm <450044101@qq.com>
2016-06-27 00:52:35 +0800

    更新服务器地址

--- app/src/main/java/com/shyling/healthmanager/util/Optional.java
+++ app/src/main/java/com/shyling/healthmanager/util/Optional.java
@@ -79,7 +79,7 @@ public class Optional<T> {
     }
 
     @Override
-    public boolean equals(Optional other){
+    public boolean equals(Object other) {
         //todo
         return false;
     }
